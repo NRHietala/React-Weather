74f8deb3610af13aadb5a2a675810f11fc1840c9
Nathan Hietala <Nrhietala@gmail.com>
2021-06-16 22:27:44 -0500

    Edited file structure

---
+++ src/pages/Home/HomeElements.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  background: green;
+`;
+
+export const HomeWrapper = styled.div``;
