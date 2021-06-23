8991c0cdfabced9015b00d4ce8908c6d7f8bd141
Nathan Hietala <Nrhietala@gmail.com>
2021-06-22 22:16:39 -0500

    Fetch hits API dynamically with city name

--- src/redux/weatherSlice.js
+++ src/redux/weatherSlice.js
@@ -1,15 +1,17 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const getWeather = createAsyncThunk("weather/getWeather", async () => {
-  // hard coded for now
-  const response = await fetch(
-    `api.openweathermap.org/data/2.5/weather?q=esko&appid=${process.env.REACT_APP_API_KEY}&units=imperial`
-  );
-  if (response.ok) {
-    const weather = await response.json();
-    return { weather };
+export const getWeather = createAsyncThunk(
+  "weather/getWeather",
+  async payload => {
+    const response = await fetch(
+      `api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`
+    );
+    if (response.ok) {
+      const weather = await response.json();
+      return { weather };
+    }
   }
-});
+);
 
 export const weatherSlice = createSlice({
   name: "weather",
