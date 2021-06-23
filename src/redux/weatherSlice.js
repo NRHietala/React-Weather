1587f9d10a4f7a571ddcb4e3b204f1d769554077
Nathan Hietala <Nrhietala@gmail.com>
2021-06-22 22:34:17 -0500

    Fetch returns weather data, disabled FAST_REFRESH due to dev server not reflecting code changes

--- src/redux/weatherSlice.js
+++ src/redux/weatherSlice.js
@@ -4,11 +4,12 @@ export const getWeather = createAsyncThunk(
   "weather/getWeather",
   async payload => {
     const response = await fetch(
-      `api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`
+      `http://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`
     );
     if (response.ok) {
       const weather = await response.json();
-      return { weather };
+      console.log(weather);
+      return weather;
     }
   }
 );
@@ -17,7 +18,15 @@ export const weatherSlice = createSlice({
   name: "weather",
   initialState: [],
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    [getWeather.pending]: (state, action) => {
+      console.log("fetching weather...");
+    },
+    [getWeather.fulfulled]: (state, action) => {
+      console.log("fetch successful", action.payload.weather);
+      state.push(action.payload.weather);
+    },
+  },
 });
 
 export default weatherSlice.reducer;
