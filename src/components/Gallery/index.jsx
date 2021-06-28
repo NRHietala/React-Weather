1ef7d9e27bd5c0faf25d760997d70a3d8645c203
Nathan Hietala <Nrhietala@gmail.com>
2021-06-28 11:55:13 -0500

    Adds state persistance in local storage

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -4,31 +4,31 @@ import { WeatherCard } from "../index";
 import { GalleryContainer, GalleryWrapper } from "./GalleryElements";
 
 const Gallery = () => {
-  const weather = useSelector(state => state.weather);
-
-  /////////////////////////////////////
-  // Need local storage persistance
-  /////////////////////////////////////
+  const weather = useSelector(state => state.weatherReducer);
 
   return (
     <GalleryContainer>
       <GalleryWrapper>
-        {weather.map(weather => (
-          <WeatherCard
-            key={weather.id}
-            sunrise={weather.sys.sunrise}
-            sunset={weather.sys.sunset}
-            cityName={weather.name}
-            dateTime={weather.dt}
-            weatherIcon={weather.weather[0].icon}
-            minTemp={weather.main.temp_min}
-            maxTemp={weather.main.temp_max}
-            temp={weather.main.temp}
-            description={weather.weather[0].description}
-            realFeel={weather.main.feels_like}
-            humidity={weather.main.humidity}
-          />
-        ))}
+        {weather ? (
+          weather.map(weather => (
+            <WeatherCard
+              key={weather.id}
+              sunrise={weather.sys.sunrise}
+              sunset={weather.sys.sunset}
+              cityName={weather.name}
+              dateTime={weather.dt}
+              weatherIcon={weather.weather[0].icon}
+              minTemp={weather.main.temp_min}
+              maxTemp={weather.main.temp_max}
+              temp={weather.main.temp}
+              description={weather.weather[0].description}
+              realFeel={weather.main.feels_like}
+              humidity={weather.main.humidity}
+            />
+          ))
+        ) : (
+          <></>
+        )}
       </GalleryWrapper>
     </GalleryContainer>
   );
--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
